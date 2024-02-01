7a930434605f70dd8454f7afa84eeb3c646f680f
Saroj Shrestha <saroj717@yahoo.com>
2024-02-01 10:30:41 -0700

    some changes

--- src/script.js
+++ src/script.js
@@ -78,3 +78,7 @@ class SignupForm extends React.Component {
     );
   }
 }
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<SignupForm/>);
